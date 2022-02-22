336edb78eb94e8716921fbed517358ae1576ae4f
Manuel Gerolin <manuel31gerolin@gmail.com>
2022-02-22 16:12:09 +0100

    ora i km posso essere aggiunti anche con decimali

--- js/main.js
+++ js/main.js
@@ -10,24 +10,29 @@
     >L'output del prezzo finale va visualizzato con massimo due decimali, per indicare centesimi sul prezzo.
 */
 
-/* chiedo all'utente quanti chilometri vuole fare */
+
+//#definisco le costanti
 
 const priceKm = 0.21;
-
 const discountUnderage = (0.80); //che posi scrivere la cifra direttamente dentro l'if o 20/100 è uguale
-
 const discountOverage = (0.60);
 
-const totalKm = parseInt(prompt("quanti chilometri devi percorrere?"));
+//#variabili da utente
+
+const totalKm = parseFloat(prompt("Quanti chilometri devi percorrere?"));
 console.log(totalKm);
 
 const age = parseInt(prompt("Quanti anni hai?"));
 console.log(age);
 
+//# processo di calcolo 
+
+let ticketPrice;
+
 if (age < 18) {
     ticketPrice = ( (priceKm * totalKm) * discountUnderage); 
     console.log(ticketPrice);
-} else if  (age >=65) {
+} else if  (age > 65) {
     ticketPrice = ( (priceKm * totalKm) * discountOverage);
     console.log(ticketPrice);
 } else {
@@ -37,4 +42,9 @@ if (age < 18) {
 
 let outPutPrice = (ticketPrice).toFixed(2);
 console.log(outPutPrice);
-document.getElementById('i-like-train').innerHTML = outPutPrice;
+document.getElementById('i-like-train').innerHTML = `Il tuo biglietto del treno ti verrà a costare  ${ outPutPrice } &euro;` ;
+
+if (isNaN(totalKm) || isNaN(age)) {
+    document.getElementById('i-like-train').innerHTML = `Hai inserito un valore non valido, ricarica la pagina e riprova`;
+};
+
